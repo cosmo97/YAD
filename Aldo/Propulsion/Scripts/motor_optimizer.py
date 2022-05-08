corrected resistance formula, added TODO fct

Aldo/Propulsion/Scripts/motor_optimizer.py
@@ -28,7 +28,8 @@ DEFAULT_NOLOAD_CURRENT = 0.0  # Ampere
 
 
 def solve_default_resistance(motor_diameter, motor_height):
-    estimated_resistance = -0.00023654 * motor_diameter * motor_height
+    estimated_resistance = -9.167414568654554e-06 * \
+        np.pi * motor_diameter**2 * motor_height / 4
     return estimated_resistance
 
 
@@ -175,6 +176,14 @@ def solve_equations(x,
                                  ]]))
 
     return sol.x if sol.cost < 1e-8 else [math.nan] * 4
+
+
+def compute_best_combinations(combination_df):
+    # TODO compute best 10, plot best 100 by weight, hovering time, price to look for a "knee"
+    # compute and plot custom loss function: price + hovering time + ...
+    # In order to do this, it is needed to normalize price etc so all features have same weight
+
+    pass
 
 
 def main():

Aldo/Propulsion/Scripts/resistance_regression.py
@@ -19,8 +19,8 @@ def import_data():
 
 
 def compute_volume(resistance_df):
-    resistance_df["Motor volume (mm^3)"] = resistance_df["Motor height (mm)"] * \
-        resistance_df["Motor diameter (mm)"]
+    resistance_df["Motor volume (mm^3)"] = np.pi * resistance_df["Motor height (mm)"] * \
+        (resistance_df["Motor diameter (mm)"])**2 / 4
     return resistance_df
 
 